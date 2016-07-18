68d349ced458461d9a2da32dee6bbaff7d71e387
maraviflo <maraviflo@gmail.com>
2016-07-18 09:47:57 +0200

    primer

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('btn', ['ngRoute']);
+
+/*app.config (['$routeProvider',function ($routeProvider) {
+	$routeProvider
+		.when ('/',{
+			templateUrl: 'views/index.html'
+		})
+
+
+
+
+
+
+
+
+}])*/
+
+app.controller('btn',['$scope','$http', function($scope,$http){
+    $scope.btn = 'button';
+    
+    $http.get("json/btn.json").success (function (data){
+        $scope.btn = data;
+    });
+}]);
